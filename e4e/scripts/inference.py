55bbe89c1bb653984213a424953fc2923d8280b8
Bhargav-567 <127102989+Bhargav-567@users.noreply.github.com>
2025-03-30 10:48:00 +0530

    change in inference.py

--- e4e/scripts/inference.py
+++ e4e/scripts/inference.py
@@ -4,19 +4,16 @@ import torch
 import numpy as np
 import sys
 import os
-import dlib
 
 sys.path.append(".")
 sys.path.append("..")
 
-from configs import data_configs, paths_config
+from configs import data_configs
 from datasets.inference_dataset import InferenceDataset
 from torch.utils.data import DataLoader
 from utils.model_utils import setup_model
 from utils.common import tensor2im
-from utils.alignment import align_face
 from PIL import Image
-
 
 def main(args):
     net, opts = setup_model(args.ckpt, device)
@@ -25,7 +22,6 @@ def main(args):
     generator.eval()
     args, data_loader = setup_data_loader(args, opts)
 
-    # Check if latents exist
     latents_file_path = os.path.join(args.save_dir, 'latents.pt')
     if os.path.exists(latents_file_path):
         latent_codes = torch.load(latents_file_path).to(device)
@@ -36,18 +32,14 @@ def main(args):
     if not args.latents_only:
         generate_inversions(args, generator, latent_codes, is_cars=is_cars)
 
-
 def setup_data_loader(args, opts):
     dataset_args = data_configs.DATASETS[opts.dataset_type]
     transforms_dict = dataset_args['transforms'](opts).get_transforms()
     images_path = args.images_dir if args.images_dir is not None else dataset_args['test_source_root']
     print(f"images path: {images_path}")
-    align_function = None
-    if args.align:
-        align_function = run_alignment
+    
     test_dataset = InferenceDataset(root=images_path,
                                     transform=transforms_dict['transform_test'],
-                                    preprocess=align_function,
                                     opts=opts)
 
     data_loader = DataLoader(test_dataset,
@@ -62,7 +54,6 @@ def setup_data_loader(args, opts):
         args.n_sample = len(test_dataset)
     return args, data_loader
 
-
 def get_latents(net, x, is_cars=False):
     codes = net.encoder(x)
     if net.opts.start_from_latent_avg:
@@ -74,7 +65,6 @@ def get_latents(net, x, is_cars=False):
         codes = codes[:, :16, :]
     return codes
 
-
 def get_all_latents(net, data_loader, n_images=None, is_cars=False):
     all_latents = []
     i = 0
@@ -82,19 +72,16 @@ def get_all_latents(net, data_loader, n_images=None, is_cars=False):
         for batch in data_loader:
             if n_images is not None and i > n_images:
                 break
-            x = batch
-            inputs = x.to(device).float()
-            latents = get_latents(net, inputs, is_cars)
+            x = batch.to(device).float()
+            latents = get_latents(net, x, is_cars)
             all_latents.append(latents)
             i += len(latents)
     return torch.cat(all_latents)
-
 
 def save_image(img, save_dir, idx):
     result = tensor2im(img)
     im_save_path = os.path.join(save_dir, f"{idx:05d}.jpg")
     Image.fromarray(np.array(result)).save(im_save_path)
-
 
 @torch.no_grad()
 def generate_inversions(args, g, latent_codes, is_cars):
@@ -107,27 +94,16 @@ def generate_inversions(args, g, latent_codes, is_cars):
             imgs = imgs[:, :, 64:448, :]
         save_image(imgs[0], inversions_directory_path, i + 1)
 
-
-def run_alignment(image_path):
-    predictor = dlib.shape_predictor(paths_config.model_paths['shape_predictor'])
-    aligned_image = align_face(filepath=image_path, predictor=predictor)
-    print("Aligned image has shape: {}".format(aligned_image.size))
-    return aligned_image
-
-
 if __name__ == "__main__":
     device = "cuda"
 
     parser = argparse.ArgumentParser(description="Inference")
-    parser.add_argument("--images_dir", type=str, default=None,
-                        help="The directory of the images to be inverted")
-    parser.add_argument("--save_dir", type=str, default=None,
-                        help="The directory to save the latent codes and inversion images. (default: images_dir")
-    parser.add_argument("--batch", type=int, default=1, help="batch size for the generator")
-    parser.add_argument("--n_sample", type=int, default=None, help="number of the samples to infer.")
-    parser.add_argument("--latents_only", action="store_true", help="infer only the latent codes of the directory")
-    parser.add_argument("--align", action="store_true", help="align face images before inference")
-    parser.add_argument("ckpt", metavar="CHECKPOINT", help="path to generator checkpoint")
+    parser.add_argument("--images_dir", type=str, default=None, help="Directory of images to be inverted")
+    parser.add_argument("--save_dir", type=str, default=None, help="Directory to save outputs")
+    parser.add_argument("--batch", type=int, default=1, help="Batch size for the generator")
+    parser.add_argument("--n_sample", type=int, default=None, help="Number of samples to infer")
+    parser.add_argument("--latents_only", action="store_true", help="Infer only the latent codes")
+    parser.add_argument("ckpt", metavar="CHECKPOINT", help="Path to generator checkpoint")
 
     args = parser.parse_args()
     main(args)
